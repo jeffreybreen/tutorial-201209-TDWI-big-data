34b900c55398d08b06301f9e8a327a5c906b9757
Jeffrey Breen <jeffrey@atmosgrp.com>
2012-09-17 14:49:55 -0700

    added trailing slash to data/ subdirectories

--- bin/populate.hdfs.sh
+++ bin/populate.hdfs.sh
@@ -4,8 +4,8 @@
 
 /usr/bin/hadoop fs -mkdir /user/cloudera/wordcount
 /usr/bin/hadoop fs -mkdir /user/cloudera/wordcount/data
-/usr/bin/hadoop fs -put data/hadoop/wordcount/* /user/cloudera/wordcount/data
+/usr/bin/hadoop fs -put data/hadoop/wordcount/* /user/cloudera/wordcount/data/
 
 /usr/bin/hadoop fs -mkdir /user/cloudera/airline
 /usr/bin/hadoop fs -mkdir /user/cloudera/airline/data
-/usr/bin/hadoop fs -put data/hadoop/airline/* /user/cloudera/airline/data
+/usr/bin/hadoop fs -put data/hadoop/airline/* /user/cloudera/airline/data/
